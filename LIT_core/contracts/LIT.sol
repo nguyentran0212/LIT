70c0b2366f9238c64269896eae975ad8732eb516
Tran Nguyen <trannguyen@Trans-iMac.local>
2019-07-03 18:49:56 +0930

    Update LIT.sol and TestSite.sol with proper way of interaction between contracts.

--- LIT_core/contracts/LIT.sol
+++ LIT_core/contracts/LIT.sol
@@ -1,7 +1,5 @@
 pragma solidity ^0.5.0;
 
-
-
 contract LIT {
     // This struct represents an experiment to be conducted by LIT
     struct Experiment {
@@ -106,6 +104,8 @@ contract LIT {
         emit experimentAssigned(_expID);
     }
     
+    // FIXME:
+    // Add support for other experiment outcomes. 
     function updateExperiment(bytes20 _expID, string memory _result) public {
         // VERSION 0.1 DOES NOT HANDLE EXPERIMENT FAILURE. CODE WILL BE UPDATED WITH ALTERNATIVE SCENARIOS IN LATER VERSIONS
 
@@ -139,37 +139,11 @@ contract LIT {
 }
 
 contract TestSite {
-    // Address of LIT core for registration and call back
     address addrLIT;
     LIT lit;
-    // Address of the owner of the testsite
     address addrOwner;
-    // This string describe the capability of a testsite. Will update in the future with more description
     string capability;
-    
-    // This event is for interacting with the client library on the infrastructure
     event experimentStaged(bytes20 expID, string code);
-    
-    constructor (string memory _capability, address _addrLIT) public {
-        addrLIT = _addrLIT;
-        lit = LIT(addrLIT);
-        capability = _capability;
-        addrOwner = msg.sender;
-        
-        // Register with LIT
-        lit.registerTestSite(_capability);
-    }
-    
-    function assignExperiment(bytes20 _expID) public{
-        // Get a reference of the LIT to access the experiment record
-        // (bytes20 id, address creator, string memory input, string memory measurement, string memory code) = lit.experiments(_expID);
-        (, , , , , , , string memory code) = lit.experiments(_expID);
-        // string memory code = "busy box";
-        // emit experimentStaged(_expID, code);
-        emit experimentStaged(_expID, code);
-    }
-    
-    function updateExperimentResult(bytes20 _expID, string memory _result) public {
-        lit.updateExperiment(_expID, _result);
-    }
+    function assignExperiment(bytes20 _expID) public;
+    function updateExperimentResult(bytes20 _expID, string memory _result) public;
 }
--- LIT_core/contracts/TestSite.sol
+++ LIT_core/contracts/TestSite.sol
@@ -82,92 +82,10 @@ contract LIT {
     
     enum ExperimentType {Created, Submitted, Assigned, Completed}
 
-    function registerTestSite(string memory _capability) public {
-        // capabilityTestSites[stringToBytes32(_capability)].push(msg.sender);
-        testSites.push(msg.sender);
-    }
-    
-    function getNumberOfResults(bytes20 _expID) public view returns (uint) {
-        return results[_expID].length;
-    }
-    
-    function getExperimentResult(bytes20 _expID, uint index) public view returns (address, string memory) {
-        return (results[_expID][index].respondedTestSite, results[_expID][index].result);
-    }
-    
-    function createExperiment(string memory _homepage, string memory _relatedArtifact, string memory _input, string memory _measurement, string memory _code) public returns(bytes20 expID) {
-        address userAddr = msg.sender;
-        
-        //Create expID by hashing the sender address with a nonce
-        expID = bytes20(keccak256(abi.encodePacked(userAddr, userNonce[userAddr])));
-        userNonce[userAddr]++;
-        userExperiments[userAddr].push(expID);
-        
-        // Create and store an experiment struct
-        experiments[expID] = Experiment(expID, 0, userAddr, _homepage, _relatedArtifact, _input, _measurement, _code);
-        
-        // Emit an event to acknowledge that the experiemnt has been submitted.
-        emit experimentSubmitted(expID);
-        
-        // FOR SIMPLICITY, VERSION 0.1 CALL CONDUCT EXPERIMENT DIRECTLY.
-        conductExperiment(expID);
-        // Return the expID;
-        return expID;
-    }
-    
-    function conductExperiment(bytes20 _expID) public {
-        // Check if the experiment ID exist
-        require(experiments[_expID].id != 0, "The specified experiment does not exist.");
-        // Check if the caller is the owner of the experiment
-        // require(msg.sender == experiments[_expID].creator, "Only the creator of an experiment can start it.");
-        
-        // Find test sites that can satisfy the experiment input and measurement requirements
-        // TEMPORARY DISABLE MAPPING LOOK UP DUE TO SECURITY ISSUE WITH INLINE ASSEMBLY
-        // address[] memory relevantSites = capabilityTestSites[stringToBytes32(experiment.measurement)];
-
-        // Assign the experiment to all registered testsites for now.
-        address[] memory relevantSites = testSites;
-        
-        // For each relevant test site
-        for (uint i = 0; i < relevantSites.length; i++) {
-            // Call assign experiment from testSite
-            TestSite ts = TestSite(relevantSites[i]);
-            ts.assignExperiment(_expID);
-        }
-        
-        // Update the state of experiment
-        experiments[_expID].state = 1;
-        emit experimentAssigned(_expID);
-    }
-    
-    function updateExperiment(bytes20 _expID, string memory _result) public {
-        // VERSION 0.1 DOES NOT HANDLE EXPERIMENT FAILURE. CODE WILL BE UPDATED WITH ALTERNATIVE SCENARIOS IN LATER VERSIONS
-
-        // Check if the expID exist
-        require(experiments[_expID].id != 0, "The specified experiment does not exist.");
-        
-        // Check if the caller is the assigned testsite
-        // TBA
-        
-        // Update the experiment
-        address testSiteAddr = msg.sender;
-        experiments[_expID].state = 2;
-        results[_expID].push(ExperimentResult(testSiteAddr, _result));
-        
-        emit experimentCompleted(_expID);
-    }
-    
-    
-    // Utility functions
-    // STOP USING THIS FUNCTION DUE TO INLINE ASSEMPLY ISSUE
-    // function stringToBytes32(string memory source) private returns (bytes32 result) {
-    //     bytes memory tempEmptyStringTest = bytes(source);
-    //     if (tempEmptyStringTest.length == 0) {
-    //         return 0x0;
-    //     }
-    
-    //     assembly {
-    //         result := mload(add(source, 32))
-    //     }
-    // }
+    function registerTestSite(string memory _capability) public;
+    function getNumberOfResults(bytes20 _expID) public view returns (uint);
+    function getExperimentResult(bytes20 _expID, uint index) public view returns (address, string memory);
+    function createExperiment(string memory _homepage, string memory _relatedArtifact, string memory _input, string memory _measurement, string memory _code) public returns(bytes20 expID);
+    function conductExperiment(bytes20 _expID) public;
+    function updateExperiment(bytes20 _expID, string memory _result) public;
 }
